175653a502ae2076f5d35242042b184574dabdad
David Egan <david@carawebs.com>
2017-03-31 19:31:26 +0100

    Remove unnecessary code.

--- src/RegisterSetting.php
+++ src/RegisterSetting.php
@@ -17,7 +17,7 @@ class RegisterSetting extends Page
         register_setting(
             $args['option_group'],
             $args['option_name'],
-            [$this, 'sanitizeCallback']//$args['option_args']
+            [$this, 'sanitizeCallback']
         );
         $this->option_name = $args['option_name'];
     }
@@ -25,27 +25,19 @@ class RegisterSetting extends Page
     public function sanitizeCallback($input)
     {
         $output = [];
-        // Loop through each of the incoming options
         foreach( $input as $key => $value ) {
-
             // Check to see if the current option has a value. If so, process it.
-            if( isset( $input[$key] ) ) {
-
+            if(isset($input[$key])) {
                 // Strip all HTML and PHP tags and properly handle quoted strings
-                $output[$key] = strip_tags( stripslashes( $input[ $key ] ) );
-
+                $output[$key] = strip_tags(stripslashes($input[$key]));
             }
-
         }
 
         // @TODO Check that this is necessary!! See `addOptionsPage`
-        array_merge(get_option($this->option_name), $output);
+        //array_merge(get_option($this->option_name), $output);
 
         // Return the array processing any additional functions filtered by this action
         // @TODO check this
         return apply_filters( 'sandbox_theme_validate_input_examples', $output, $input );
-
-
     }
-
 }
